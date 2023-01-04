cb0e3f22c9c8b32362220c0660d7f5e880b95621
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-01-04 11:17:15 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m AaruRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE AARU Robot
+:: Enables virtual env mode and then starts Fallen
+env\scripts\activate.bat && py -m AaruRobot
